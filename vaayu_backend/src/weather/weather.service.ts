a7eda841815517f3116c2326a884b019464f3c72
Sushant <sushantkumaryadav912@gmail.com>
2025-09-27 11:41:36 +0530

    modified the components

--- vaayu_backend/src/weather/weather.service.ts
+++ vaayu_backend/src/weather/weather.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class WeatherService {}
+export class WeatherService {
+  constructor(private readonly http: HttpService) {}
+
+  async getForecast(lat: number, lon: number, timestamp?: string) {
+    const url = `${process.env.AI_SERVICE_URL || 'http://localhost:8000'}/predict`;
+    const payload = { latitude: lat, longitude: lon, timestamp };
+    const { data } = await firstValueFrom(this.http.post(url, payload));
+    return data;
+  }
+}
